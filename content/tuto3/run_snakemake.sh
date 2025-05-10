0ecda474e6b116edb7d189204ceb88be3dec777e
Pierre Poulain <pierre.poulain@cupnet.net>
2025-05-10 17:29:34 +0200

    Update snakemake scripts

--- content/tuto3/run_snakemake.sh
+++ content/tuto3/run_snakemake.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Script will stop at the first error
 set -euo pipefail
@@ -8,8 +8,8 @@ NJOBS=30
 
 # Load modules
 module load fastqc/0.11.9
-module load star/2.7.9a
-module load samtools/1.14
+module load star/2.7.10b
+module load samtools/1.15.1
 module load htseq/0.13.5
 module load cufflinks/2.2.1
 
@@ -17,7 +17,7 @@ module load snakemake/7.25.0
 module load slurm-drmaa/1.0.8
 
 # Paths
-export DATA_DIR="/shared/projects/202304_duo/data/rnaseq_scere"
+export DATA_DIR="/shared/projects/2501_duo/data/rnaseq_scere"
 
 # Run snakemake with cluster mode
 snakemake --profile snakemake_profiles/cluster/ \
